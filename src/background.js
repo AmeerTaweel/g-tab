98610f3852c9f3abe4a2fc14e13b9278455df7aa
Ameer Taweel <ameertaweel2002@gmail.com>
2019-05-24 17:05:51 +0300

    style: Fixing formatting problems in background.js

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("gTab has been installed.");
-    chrome.storage.sync.set({groups: []}, () => {
-      console.log("gTab has been initialized.");
-    });
-});
+  console.log(`gTab has been installed.`)
+  chrome.storage.sync.set({ groups: [] }, () => {
+    console.log(`gTab has been initialized.`)
+  })
+})
